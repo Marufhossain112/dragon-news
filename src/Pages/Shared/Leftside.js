f0d98a788e703360f02861ca446b0ec4eb4fac33
maruf <dev.maruf112@gmail.com>
2022-10-20 23:23:34 +0600

    update

---
+++ src/Pages/Shared/Leftside.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Leftside = () => {
+  return (
+    <div>
+      <h2>This is leftSide.</h2>
+    </div>
+  );
+};
+
+export default Leftside;
---
+++ src/Pages/Shared/rightSide.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const rightSide = () => {
+  return (
+    <div>
+      <h2>This is right side.</h2>
+    </div>
+  );
+};
+
+export default rightSide;
