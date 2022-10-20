89c585641bf72b2c19d845517afcb342118e9e88
maruf <dev.maruf112@gmail.com>
2022-10-21 02:57:42 +0600

    data load successful

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const LeftSideNav = () => {
+  const [Categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/news-categories")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div>
       <h2>This is Left</h2>
