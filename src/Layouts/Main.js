a2c3532ae41fe647ea482f97f2b830ad45d1e4ef
maruf <dev.maruf112@gmail.com>
2022-10-21 02:07:06 +0600

    update 2.07

--- src/Layouts/Main.js
+++ src/Layouts/Main.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
-import Footer from "../Pages/Shared/Footer";
-import Header from "../Pages/Shared/Header";
+import LeftSideNav from "../Pages/Shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../Pages/Shared/RightSideNav";
 
 const Main = () => {
   return (
     <div>
       <Container>
         <Row>
-          <Col lg="2">Side Nav</Col>
+          <Col lg="2">
+            <LeftSideNav></LeftSideNav>
+          </Col>
           <Col lg="7">
             <Outlet></Outlet>
           </Col>
-          <Col lg="3">Right Side</Col>
+          <Col lg="3">
+            <RightSideNav></RightSideNav>
+          </Col>
         </Row>
       </Container>
     </div>
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
+import Categories from "../Pages/Categories/Categories";
+import News from "../Pages/News/News";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+  },
+  {
+    path: "/category/:id",
+    element: <Categories></Categories>,
+  },
+  {
+    path: "/news/:id",
+    element: <News></News>,
   },
 ]);
