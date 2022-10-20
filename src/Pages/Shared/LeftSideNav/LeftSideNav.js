3fc94a52b7d90317cb79129334ba213ce22d02d9
maruf <dev.maruf112@gmail.com>
2022-10-21 05:59:43 +0600

    leftside done

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-  const [Categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     fetch("http://localhost:5000/news-categories")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setCategories(data));
   }, []);
   return (
     <div>
-      <h2>This is Left</h2>
+      <div>
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
