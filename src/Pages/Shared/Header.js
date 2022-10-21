8c7e077d3313f3c30b1fc3fbc163418da8858d0d
maruf <dev.maruf112@gmail.com>
2022-10-21 08:33:57 +0600

    responsive navbar created

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -3,32 +3,30 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+import LeftSideNav from "./LeftSideNav/LeftSideNav";
 
 const Header = () => {
   return (
     <div>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            {" "}
+            <Link to="/">Dragon News 24 </Link>{" "}
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#link">Categories</Nav.Link>
+              <Nav.Link href="#news">News</Nav.Link>
             </Nav>
+            {
+              <div className="d-lg-none">
+                <LeftSideNav></LeftSideNav>
+              </div>
+            }
           </Navbar.Collapse>
         </Container>
       </Navbar>
